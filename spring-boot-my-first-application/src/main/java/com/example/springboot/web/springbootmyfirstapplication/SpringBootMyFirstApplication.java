1624ff6767da74bbd802fe5e43ad81275d59bd52
Alexander Sanchez <alexander.sanchez@liferay.com>
2019-03-25 21:33:57 -0500

    first commit

---
+++ spring-boot-my-first-application/src/main/java/com/example/springboot/web/springbootmyfirstapplication/SpringBootMyFirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.web.springbootmyfirstapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyFirstApplication.class, args);
+	}
+
+}
